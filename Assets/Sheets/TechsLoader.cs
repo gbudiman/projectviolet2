195596b21f67ba3ca34556d6f00fa29ed76312cf
Gloria Budiman <wahyu.g@gmail.com>
2017-12-30 04:41:06 -0800

    Add skills/techs

---
+++ Assets/Sheets/TechsLoader.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TechsLoader : MonoBehaviour {
+  Dictionary<string, SkillData> techs;
+
+	// Use this for initialization
+	void Start () {
+    string raw = System.IO.File.ReadAllText("Assets/Sheets/Techs.tsv");
+    techs = process(raw);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  public Dictionary<string, SkillData> get_techs() {
+    return techs;
+  }
+
+  Dictionary<string, SkillData> process(string raw) {
+    string[] lines = raw.Split("\n"[0]);
+    Dictionary<string, SkillData> skills = new Dictionary<string, SkillData>(); 
+
+    foreach (string line in lines) {
+      string[] cell = (line.Trim()).Split("\t"[0]);
+      string code_s = cell[0];
+      float code = -1f;
+
+      if (!float.TryParse(code_s, out code)) continue;
+      if (code < 0f) continue;
+
+      string skill_key = cell[1];
+      string skill_name = cell[2];
+      string activation = cell[3];
+      string target = cell[4];
+      string requirements = cell[5];
+      string description = cell[6];
+
+      SkillData skill_data = new SkillData(skill_key, skill_name, activation, target, requirements, description);
+      skills.Add(skill_key, skill_data);
+    }
+
+    return skills;
+  }
+}
+
+public class SkillData {
+  string skill_key;
+  string skill_name;
+  string activation;
+  string target;
+  string requirements;
+  string description;
+
+  public SkillData(string _skill_key, 
+                   string _skill_name, 
+                   string _activation, 
+                   string _target, 
+                   string _requirements, 
+                   string _description) {
+    skill_key = _skill_key;
+    skill_name = _skill_name;
+    activation = _activation;
+    target = _target;
+    requirements = _requirements;
+    description = _description;
+  }
+}
